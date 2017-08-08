cb721f029c9e06c3c969482c0e73e49b6d31482d
Manish Garg <manishgargpro@gmail.com>
2017-08-08 14:45:46 -0400

    Changed AJAX request and fixed HTML
    Determined that it is simpler to use an endpoint that enables search by state to narrow down senators and gives clear and concise JSON objects that can be used for twitter search. However, problem exists with cross-domain xhttp requests and CORS, will need to solve before moving forward.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,38 +1,29 @@
-//api params
-var senEndpoint = "115/Senate/members";
-var senURL = "https://api.propublica.org/congress/v1/";
-var senList   = {
-  "async": true,
-  "crossDomain": true,
-  "url": " ",
-  "method": "GET",
-  "headers": {
-    "x-api-key": "mayRUHtP2kt3W1WNxLvumC4K1LJyC89q9PDbKwQl"
-  }
-}
-//object to hold senator data
-senObject = {};
 
-//senator search function and API call. 
-//If only first name is given will return the last matching senator it finds. Will only return one senator at present
 $("#search-button").on("click", function(e) {
-  var searchArr = $("#searchbar").val().split(" ");
-  senList.url = senURL + senEndpoint + ".json";
-  $.ajax(senList).done(function (response) {
-    var senMem = response.results[0].members;
-    //runs through list checking each senator and producing an object based on first name and last name if included
-    $.each(senMem, function(i){
-      if(senMem[i].first_name == searchArr[0]){
-        if(searchArr[1] != undefined){
-          if(senMem[i].last_name == searchArr[1]){
-            senObject = senMem[i];
-        }
-      }
-      else{
-        senObject = senMem[i];
-      }
-    }
-    });
-    console.log(senObject);
-  });
+  var searchTerm = $("#searchbar").val();
+  var senators = getSens(searchTerm);
+  console.log(senators);
 });
+
+function getSens(searchTerm) {
+  var senObject = {};
+  var senEndpoint = "congress/v1/members/senate/" + searchTerm + "/current";
+  var senURL = "https://api.propublica.org/congress/v1/";
+  var senList = {
+    "async": true,
+    "crossDomain": true,
+    "url": senURL + senEndpoint + ".json",
+    "method": "GET",
+    "headers": {
+      "x-api-key": "mayRUHtP2kt3W1WNxLvumC4K1LJyC89q9PDbKwQl"
+    }
+  }
+
+  $.ajax(senList).done(function(response) {
+    var senMem = response.results;
+    $.each(senMem, function(i) {
+        senObject.append(senMem[i]);
+        });
+    });
+  return senObject;
+}
