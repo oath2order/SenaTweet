4889592db19c3c4bc97502eff076bacb826f6ee7
Manish Garg <manishgargpro@gmail.com>
2017-08-11 15:17:07 -0400

    script.js is now one object, still more work needs to be done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,62 +1,92 @@
-var senObject = {};
-var searchTerm = "";
-var senURL = "https://api.propublica.org/congress/v1/";
-var senList = {
-  "async": true,
-  "crossDomain": true,
-  "url": "",
-  "method": "GET",
-  "headers": {
-    "x-api-key": "mayRUHtP2kt3W1WNxLvumC4K1LJyC89q9PDbKwQl"
+var searchMethod = {
+
+  firstName: "",
+  lastName: "",
+  senURL: "https://api.propublica.org/congress/v1/",
+  senList: {
+    "async": true,
+    "crossDomain": true,
+    "url": "",
+    "method": "GET",
+    "headers": {
+      "x-api-key": "mayRUHtP2kt3W1WNxLvumC4K1LJyC89q9PDbKwQl"
+    }
+  },
+
+  senEndpoint: "",
+
+  senIdArr: [],
+
+  state: "",
+
+  setEndpoints: function() {},
+
+  activateNameButton: function(button) {
+    $("button").removeClass("active");
+    $(button).addClass("active");
+    $("#states").hide();
+    $("#first-name").show();
+    $("#last-name").show();
+  },
+
+  activateStatesButton: function(button) {
+    $("button").removeClass("active");
+    $(button).addClass("active");
+    $("#first-name").hide();
+    $("#last-name").hide();
+    $("#states").show();
+  },
+
+  searchButtonMethod: function() {
+    this.firstName = $("#first-name-bar").val();
+    this.lastName = $("#last-name-bar").val();
+    this.setSenId(this.firstName, this.lastName);
+  },
+
+  setSenId: function(firstName, lastName) {
+    this.senIdArr = [];
+    if ($("#search-by-name").hasClass("active")) {
+      this.senList.url = this.senURL + "115/Senate/members.json";
+      $.ajax(this.senList).done(function(response) {
+        var senMem = response.results[0].members;
+        $.each(senMem, function(i) {
+          if (senMem[i].first_name == firstName && searchMethod.lastName == "") {
+            searchMethod.senIdArr.push(senMem[i].id);
+          } else if (senMem[i].last_name == lastName && searchMethod.firstName == "") {
+            searchMethod.senIdArr.push(senMem[i].id);
+          } else if (senMem[i].first_name == firstName && senMem[i].last_name == lastName) {
+            searchMethod.senIdArr.push(senMem[i].id);
+          } else {
+            //PLEASE replace this
+            alert("your search did not return any results");
+          }
+        });
+      });
+    } else if ($("#search-by-state").hasClass("active")) {
+      this.state = ;
+      this.senList.url = this.senURL + "members/senate/" + this.state + "/current.json";
+      $.ajax(this.senList).done(function(response) {
+        var senMem = response.results;
+        $.each(senMem, function(i) {
+          searchMethod.senIdArr.push(senMem[i].id);
+        });
+      });
+    }
+    console.log(this.senIdArr);
   }
+
 }
 
-var senEndpoint = "";
 
-function setEndpoints(searchTerm) {
-  if ($("#search-by-name").hasClass("active")) {
-    searchTerm = senId;
-    senEndpoint = "115/Senate/" + searchTerm + ".json";
-  } else if ($("#search-by-state").hasClass("active")) {
-    senEndpoint = "members/Senate/" + searchTerm + "/current.json";
-  }
-}
 
-$("#search-by-name").on("click", function() {
-  $("button").removeClass("active");
-  $(this).addClass("active");
+$("#name-button").on("click", function() {
+  searchMethod.activateNameButton(this);
 });
 
-$("#search-by-state").on("click", function() {
-  $("button").removeClass("active");
-  $(this).addClass("active");
+$("#states-button").on("click", function() {
+  searchMethod.activateStatesButton(this);
 });
 
 $("#search-button").on("click", function() {
-  searchTerm = $("#searchbar").val();
-  setSenId(searchTerm);
-  setEndpoints(searchTerm);
-  senList.url = senURL + senEndpoint;
-  console.log(senList.url);
+  searchMethod.searchButtonMethod();
 });
-
-var senId;
-
-function setSenId(searchTerm) {
-  var searchArr = searchTerm.split(" ");
-  senList.url = senURL + "115/Senate/members.json";
-  $.ajax(senList).done(function(response) {
-    var senMem = response.results[0].members;
-    $.each(senMem, function(i){
-      if(senMem[i].first_name == searchArr[0]){
-        if(searchArr[1] != undefined){
-          if(senMem[i].last_name == searchArr[1]){
-            senId = senMem[i].id;
-          }
-        }
-      } else{
-        senId = senMem[i].id;
-      }
-    });
-  });
-}
