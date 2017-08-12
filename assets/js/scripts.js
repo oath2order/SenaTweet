f936afba193388cb608a632e4d073de3e732f5fb
Josh Kasten <oath2order@gmail.com>
2017-08-11 22:35:52 -0400

    Added pictures, NYT and Firebase functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -43,6 +43,135 @@ function produceSen(senId){
     senObject = response;
     console.log(senList.url);
     console.log(senObject);
+    timesHandler.apiCall(senObject.results[0].first_name, senObject.results[0].last_name);
   });
-
 }
+
+//handles all firebasee account and database functions
+var accHandler = {
+  //user and database object variables
+  userDatabase : firebase.database(),
+  userArr : [],
+  uid : "",
+  //event listener for follow button leads here
+  senFollow : function(){
+    var senId = senObject.results[0].member_id;
+    console.log(this); 
+    if($.inArray(senId, accHandler.userArr) === -1){
+      accHandler.userDatabase.ref(accHandler.uid).push(senId);
+      accHandler.buildSenList();
+    }
+    else{
+      alert('Senator Already Followed');
+    }
+  },
+  //Will be used to render the senator bios for the user page
+  buildSenList : function(){
+  accHandler.userDatabase.ref(accHandler.uid).on("value", function(snapShot){
+      accHandler.userArr = Object.values(snapShot.val());
+      for (var i = 0; i < accHandler.userArr.length; i++) {
+        produceSen(accHandler.userArr[i]);
+      }
+    });
+  },
+  //creates user when sign up button is pressed
+  createUser: function(){
+  var email = $("#email").val();
+  var password = $("#password").val();
+  //handles error returning
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+  // Sign in with email and pass.
+  // [START createwithemail]
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {  
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+    // [END createwithemail]
+    console.log(firebase.auth().currentUser);
+},
+  //handles user sign in functionality
+  signIn: function(){
+  var email = $("#email").val();
+  var password = $("#password").val();
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    console.log("test1");
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong password.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  })
+  console.log(firebase.auth().currentUser);
+  },
+  //handles user sign out functionality
+  signOut: function(){
+    firebase.auth().signOut();
+  },
+  //handles user sign out functionality
+  initApp: function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+      accHandler.uid = user.uid;
+      accHandler.buildSenList();
+    });
+  }
+};
+
+timesHandler = {
+ apiCall : function(firstName, lastName){
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+  url += '?' + $.param({
+    'api-key': "4ef882df201e419684d1da14f37e8634",
+    'q': firstName + " " + lastName,
+    'fl': "web_url, snippet, headline"
+  });
+  console.log(url);
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    timesHandler.renderArticles(result.response.docs);
+  }).fail(function(err) {
+    throw err;
+  });
+ },
+ renderArticles: function(list){
+    $("#newsdisplay").empty();
+    for (var i = 0; i <= 2; i++) {
+      console.log(list[i])
+      $("#newsdisplay").append("<a href='" + list[i].web_url + "' target='blank'><h4 class='headline'>" 
+      + list[i].headline.main + "</h4></a><p clas='snippet'>" + list[i].snippet + "</p>")
+    }
+  }
+}
+
+
+
+document.getElementById('sign-up').addEventListener('click', accHandler.createUser, false);
+document.getElementById('sign-in').addEventListener('click', accHandler.signIn, false);
+document.getElementById('sign-out').addEventListener('click', accHandler.signOut, false);
+document.getElementById('follow').addEventListener('click', accHandler.senFollow, false);
+window.onload = function() {
+  accHandler.initApp();
+};
