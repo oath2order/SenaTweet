d339228caa2393fc3feb51b18bc10fbd567bff1b
Manish Garg <manishgargpro@gmail.com>
2017-08-12 11:27:28 -0400

    added search by party functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,8 +3,6 @@
 var searchMethod = {
 
   //all the variables are kept on top
-  firstName: "",
-  lastName: "",
   senURL: "https://api.propublica.org/congress/v1/",
   senList: {
     "async": true,
@@ -16,18 +14,23 @@ var searchMethod = {
     }
   },
 
-  senEndpoint: "",
-
   //this is what we'll need to use for our populate function outside of the search function, these are eventually what get passed into the members API; Josh, hopefully this is what you're looking for
   senIdArr: [],
 
+  firstName: "",
+
+  lastName: "",
+
   state: "",
+
+  party: "",
 
   //there are functions added for every button for every search type we are doing
   activateNameButton: function(button) {
     $("button").removeClass("active");
     $(button).addClass("active");
     $("#states").hide();
+    $("#party").hide();
     $("#first-name").show();
     $("#last-name").show();
   },
@@ -37,7 +40,17 @@ var searchMethod = {
     $(button).addClass("active");
     $("#first-name").hide();
     $("#last-name").hide();
+    $("#party").hide();
     $("#states").show();
+  },
+
+  activatePartyButton: function(button) {
+    $("button").removeClass("active");
+    $(button).addClass("active");
+    $("#first-name").hide();
+    $("#last-name").hide();
+    $("#states").hide();
+    $("#party").show();
   },
 
   //function for input validation, kept separate so that it can be called when needed, I'm using console logs in place of actual alerts for now just to test functionality, someone PLEASE replace them with modals like they're supposed to be (see below)
@@ -46,12 +59,8 @@ var searchMethod = {
       if (/^[a-zA-Z]+/.test(input)) {
         return input;
       } else {
-        //someone PLEASE change this
         console.log("invalid");
       }
-    } else if ($("input").val() == "") {
-      //someone PLEASE change this
-      console.log("your field is empty, please type something");
     }
   },
 
@@ -69,18 +78,23 @@ var searchMethod = {
             searchMethod.senIdArr.push(senMem[i].id);
           }
         });
+        console.log(searchMethod.senIdArr);
       } else if (searchMethod.lastName !== undefined && searchMethod.firstName == undefined) {
         $.each(senMem, function(i) {
           if (senMem[i].last_name.toLowerCase() == searchMethod.lastName.toLowerCase()) {
             searchMethod.senIdArr.push(senMem[i].id);
           }
         });
+        console.log(searchMethod.senIdArr);
       } else if (searchMethod.firstName !== undefined && searchMethod.lastName !== undefined) {
         $.each(senMem, function(i) {
           if (senMem[i].first_name.toLowerCase() == searchMethod.firstName.toLowerCase() && senMem[i].last_name.toLowerCase() == searchMethod.lastName.toLowerCase()) {
             searchMethod.senIdArr.push(senMem[i].id);
           }
         });
+        console.log(searchMethod.senIdArr);
+      } else if (searchMethod.firstName == undefined && searchMethod.lastName == undefined) {
+        console.log("your field is empty, please type something");
       }
     });
   },
@@ -89,11 +103,31 @@ var searchMethod = {
     this.state = $("#state-bar").text();
     console.log(this.state);
     this.senList.url = this.senURL + "members/senate/" + this.state + "/current.json";
-    $.ajax(this.senList).done(function(response) {
-      var senMem = response.results;
-      $.each(senMem, function(i) {
-        searchMethod.senIdArr.push(senMem[i].id);
+    if(this.state !== "States"){
+      $.ajax(this.senList).done(function(response) {
+        var senMem = response.results;
+        $.each(senMem, function(i) {
+          searchMethod.senIdArr.push(senMem[i].id);
+        });
+        console.log(searchMethod.senIdArr);
       });
+    } else{
+      console.log("specify a state first")
+    }
+  },
+
+  searchByParty: function(){
+    this.party = $("#party-bar").val();
+    console.log(this.party);
+    this.senList.url = this.senURL + "115/Senate/members.json";
+    $.ajax(this.senList).done(function(response) {
+      var senMem = response.results[0].members;
+      $.each(senMem, function(i) {
+        if (senMem[i].party == searchMethod.party) {
+          searchMethod.senIdArr.push(senMem[i].id);
+        }
+      });
+      console.log(searchMethod.senIdArr);
     });
   },
 
@@ -104,8 +138,9 @@ var searchMethod = {
       this.searchByName();
     } else if ($("#states-button").hasClass("active")) {
       this.searchByState();
+    } else if ($("#party-button").hasClass("active")) {
+      this.searchByParty();
     }
-    console.log(this.senIdArr);
   }
 
 }
@@ -120,8 +155,17 @@ $("#states-button").on("click", function() {
   searchMethod.activateStatesButton(this);
 });
 
+$("#party-button").on("click", function() {
+  searchMethod.activatePartyButton(this);
+});
+
 $("#states-menu li a").on("click", function() {
   $("#state-bar").text($(this).attr("value"));
+});
+
+$("#party-menu li a").on("click", function() {
+  $("#party-bar").text($(this).text())
+  $("#party-bar").val($(this).attr("value"));
 });
 
 $("#search-button").on("click", function() {
